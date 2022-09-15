5274ea7e9a933b868437199b5b666e8a1ad5a280
andri <andridwiprasa@gmail.com>
2022-09-15 09:00:56 +0700

    Landingpage2

--- src/component/LandingPage.js
+++ src/component/LandingPage.js
@@ -66,6 +66,74 @@ function LandingPage() {
             </ul>
             </div>
 
+            <div className='everiting'>
+            <div className="grid grid-cols-3 ">
+            <p className="txt-everiting">Everything You Need 
+            <p className="txt-kecil-everiting">Get the production and quality of an in-house team without the overhead cost.</p> 
+            <div className="btn-everiting">
+        <button className="tmbl-everiting">Selengkapnya </button><img  src='assets/img/btn-us.png'/>
+    </div></p>
+    <div  className="frame-everiting">
+        <div className="frame-1-everiting">
+        <div className="frame-1-img">
+            <img className="img1" src="./assets/adp/image1.png" />
+            </div>
+        <div className="frame-1">
+            <p className="txt-frame1">Unlimited revisions</p>
+            <p className="txt-frame1-kecil">Not happy yet with your design? Submit as many revisions as you need until it pops!</p>
+        </div>
+        </div>
+        <div className="frame-2-everiting">
+            <div className="frame-2-img">
+                <img className="img2" src="./assets/adp/image2.png"/>
+            </div>
+            <div className="frame-2">
+            <p className="txt-frame1">Fixed monthly rate</p>
+            <p className="txt-frame1-kecil">Why pay more? Save thousands of dollars paying by the month instead of per design.</p>
+        </div>
+    </div>
+    <div className="frame-3-everiting">
+            <div className="frame-2-img">
+                <img className="img3" src="./assets/adp/image3.png"/>
+            </div>
+            <div className="frame-2">
+            <p className="txt-frame1">1-2 day turnaround</p>
+            <p className="txt-frame1-kecil">Always know how long your designs will take. Never wait more than 2 days for designs.</p>
+        </div>
+        </div>
+    </div>
+    <div className="frame-everiting-1">
+    <div className="frame-4-everiting">
+        <div className="frame-2-img">
+            <img className="img4" src="./assets/adp/image4.png" />
+            </div>
+        <div className="frame-2">
+            <p className="txt-frame1">Brand folderss</p>
+            <p className="txt-frame1-kecil">Keep your brand’s assets on Penji so your designers can match your style.</p>
+        </div>
+        </div>
+        <div className="frame-5-everiting">
+            <div className="frame-2-img">
+                <img className="img5" src="./assets/adp/image5.png"/>
+            </div>
+            <div className="frame-2">
+            <p className="txt-frame1">Vetted designers</p>
+            <p className="txt-frame1-kecil">We take our quality seriously. Only 2% of designers that apply to Penji make the team.</p>
+        </div>
+    </div>
+    <div className="frame-6-everiting">
+            <div className="frame-3-img">
+                <img className="img6" src="./assets/adp/image6.png"/>
+            </div>
+            <div className="frame-3">
+            <p className="txt-frame1">Money-back guarantee</p>
+            <p className="txt-frame1-kecil">Not in love with Penji by 30 days? Let us know and receive a full refund, hassle-free.</p>
+        </div>
+        </div>
+        </div>
+        </div>
+    </div>
+
             <div className='banner-home'>
                 <div className='frame-txt-banner'>
                     <p className='txt-banner'>Social Media Manajemen</p>
@@ -79,11 +147,120 @@ function LandingPage() {
                 </div>
                 <div className='pict-phone'>
                         <img className = "phone1" src='assets/img/phone1.png' alt='Iconbtn'></img>
-                        <img className = "phone2" src='assets/img/phone2.png' alt='Iconbtn'></img>
+                        <img className = "phone2" src='assets/adp/hp2.png' alt='Iconbtn'></img>
+                        <img className='layar2' src='assets/adp/layar2.png' alt='Iconbtn'/>
+                        <img className='phone3' src='assets/adp/hp3.png' alt='Iconbtn'/>
+                        <img className='layar3' src='assets/adp/layar3.png' alt='Iconbtn'/>
+                        <img className='phone4' src='assets/adp/hp4.png' alt='Iconbtn'/>
+                        <img className='layar4' src='assets/adp/layar4.png' alt='Iconbtn'/>
                     </div>
             </div>
+
+            <p className='txt-plans'>PLANS THAT WORK FOR YOU</p> 
+            <div className='subs'>
+                <div className='subs1'>
+                    <div className='subs1-harga'>
+                        <p className='txt-harga'>Bulanan</p>
+                        <p className='txt-harga1'>Hanya Rp 5.000.000</p>
+                        <p className='txt-harga2'>Rp.1.500.000</p>
+                        <p className='txt-harga3'>/ Month</p>
+                    </div>
+                    <div className='btn-harga'>
+                    <button className='txt-btn-harga'>PILIH</button>
+                    </div>
+                <p className='rectangle'></p>
+                
+                <div className='layanan-harga'>
+                    <div className='frame-layanan-harga'>
+                        <p className='txt-frame-harga'>Yang kamu dapat</p>
+                        </div>
+                    <div className='frame-layanan-harga1'>
+                            <img src='assets/adp/icon-ckls.png'/>
+                            <p className='txt-frame-harga1'>1 Bulan Durasi</p>       
+                            </div> 
+                    <div className='frame-layanan-harga2'>
+                        <img src='assets/adp/icon-ckls.png'/>
+                        <p className='txt-frame-harga2'>Dedicated Designer Team</p>
+                                </div>
+                    <div className='frame-layanan-harga3'>
+                        <img src='assets/adp/icon-ckls.png'/>
+                        <p className='txt-frame-harga3'>Unlimited Revision</p>
+                                </div>            
+                            </div>
+                        </div>
+
+                <div className='subs2'>
+                    <div className='subs1-harga'>
+                        <p className='txt-harga'>Pro</p>
+                        <p className='txt-harga1'>Hanya Rp 5.000.000</p>
+                        <p className='txt-harga2'>Rp.1.500.000</p>
+                        <p className='txt-harga3'>/ Month</p>
+                    </div>
+                    <div className='btn-harga1'>
+                    <button className='txt-btn-harga1'>PILIH</button>
+                    </div>
+                <p className='rectangle'></p>
+                <div className='layanan-harga'>
+                    <div className='frame-layanan-harga'>
+                        <p className='txt-frame-harga'>Yang kamu dapat</p>
+                        </div>
+                    <div className='frame-layanan-harga1'>
+                            <img src='assets/adp/icon-ckls.png'/>
+                            <p className='txt-frame-harga1'>1 Bulan Durasi</p>       
+                            </div> 
+                    <div className='frame-layanan-harga2'>
+                        <img src='assets/adp/icon-ckls.png'/>
+                        <p className='txt-frame-harga2'>Dedicated Designer Team</p>
+                                </div>
+                    <div className='frame-layanan-harga3'>
+                        <img src='assets/adp/icon-ckls.png'/>
+                        <p className='txt-frame-harga3'>Unlimited Revision</p>
+                                </div>            
+                            </div>
+                        </div>
+
+                    <div className='subs3'>
+                    <div className='subs1-harga'>
+                        <p className='txt-harga'>Mingguan</p>
+                        <p className='txt-harga1'>Hanya Rp 5.000.000</p>
+                        <p className='txt-harga2'>Rp.1.500.000</p>
+                        <p className='txt-harga3'>/ Month</p>
+                    </div>
+                    <div className='btn-harga'>
+                    <button className='txt-btn-harga'>PILIH</button>
+                    </div>
+                <p className='rectangle'></p>
+                <div className='layanan-harga'>
+                    <div className='frame-layanan-harga'>
+                        <p className='txt-frame-harga'>Yang kamu dapat</p>
+                        </div>
+                    <div className='frame-layanan-harga1'>
+                            <img src='assets/adp/icon-ckls.png'/>
+                            <p className='txt-frame-harga1'>1 Bulan Durasi</p>       
+                            </div> 
+                    <div className='frame-layanan-harga2'>
+                        <img src='assets/adp/icon-ckls.png'/>
+                        <p className='txt-frame-harga2'>Dedicated Designer Team</p>
+                                </div>
+                    <div className='frame-layanan-harga3'>
+                        <img src='assets/adp/icon-ckls.png'/>
+                        <p className='txt-frame-harga3'>Unlimited Revision</p>
+                                </div>            
+                            </div>
+                        </div>    
+                    </div>
+
+                    <p className='txt-ifyou'>If you can imagine it, we can design it.</p>
+                    <div className='btn-ifyou'><button className='txt-btn-ifyou'>COBASEKARANG</button></div>
+                    <div className='container-slider'>
+                    <img className='kanan' src='assets/adp/kanan.png'/>
+                    <img className='rektengle1' src='assets/adp/rektengle1.png'/>
+                    <img className='rektengle2' src='assets/adp/rektangle3.png'/>
+                    <img className='rektengle3' src='assets/adp/rektangle2.png'/>
+                    <img className='kiri' src='assets/adp/kiri.png'/>
+                    </div>  
+            </div>
         </div>
-    </div>
   )
 }
 
