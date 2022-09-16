28df87fbb90c9ee7f22f5216ee2ad992cc7dcb6f
adityailhamss <adityailham193@gmail.com>
2022-09-16 19:49:08 +0700

    BookingPage

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import Portfolio from './component/Portfolio';
 import Demo from './component/Demo';
 import StepTwo from './component/StepTwo';
 import StepThree from './component/StepThree';
-<<<<<<< HEAD
 import VideoDemo from './component/VideoDemo';
 import CalendarPopup from './component/CalendarPopup';
 import React, { useState } from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-=======
-import Order from './component/Order';
-import Paket from './component/Paket';
->>>>>>> 5274ea7e9a933b868437199b5b666e8a1ad5a280
 
 function App() {
   const [showMyModal, setShowMyModal] = useState(false)
   return (
-<<<<<<< HEAD
     <Router>
       <Routes>
         <Route path="/demo" element={<Demo/>}/>
@@ -25,12 +19,6 @@ function App() {
         <Route path="/stepthree" element={<StepThree/>}/>
       </Routes>
     </Router>
-=======
-
-      <LandingPage />
-      
-      
->>>>>>> 5274ea7e9a933b868437199b5b666e8a1ad5a280
 
   );
 }
