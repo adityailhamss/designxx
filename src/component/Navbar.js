72814f161da3c6afa6ca11ee9ea697dbdd88d219
maikeljh <mjh191003@gmail.com>
2022-09-19 19:31:45 +0700

    add navbar and active style for navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,61 @@
+import Logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex flex-row px-14 items-center">
+        <div>
+          <NavLink to={"/"}>
+            <img src={Logo} alt="" />
+          </NavLink>
+        </div>
+        <ul className="flex flex-row ml-auto mr-20">
+          <li className="py-8">
+            <NavLink
+              to={"/"}
+              className="py-8 px-6 tracking-widest"
+              style={(isActive) => ({
+                color: isActive.isActive ? "#E9520B" : "#C4C4C4",
+              })}
+            >
+              HOME
+            </NavLink>
+          </li>
+          <li className="py-8">
+            <NavLink
+              to={"/portofolio"}
+              className="py-8 px-6 tracking-widest"
+              style={(isActive) => ({
+                color: isActive.isActive ? "#E9520B" : "#C4C4C4",
+              })}
+            >
+              PORTOFOLIO
+            </NavLink>
+          </li>
+          <li className="py-8">
+            <NavLink
+              to={"/HowItWorks"}
+              className="py-8 px-6 tracking-widest"
+              style={(isActive) => ({
+                color: isActive.isActive ? "#E9520B" : "#C4C4C4",
+              })}
+            >
+              HOW IT WORK
+            </NavLink>
+          </li>
+          <li className="py-8 px-6">
+            <NavLink
+              to={"/demo"}
+              className="py-1 px-4 bg-[#E9520B] text-white rounded"
+            >
+              <button className="tracking-widest">BOOK A DEMO</button>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
